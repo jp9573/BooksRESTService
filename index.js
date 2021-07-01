6b9384be6a34e7c85967ce00ff84b252ce1ab47d
Jay Patel <jp9573@gmail.com>
2021-07-01 20:29:34 -0300

    Add book router

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
+bookRouter.route("/books").get((req, res) => {
+  const response = { hello: "World!" };
+
+  res.json(response);
+});
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");

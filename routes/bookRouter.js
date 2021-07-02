e3dd491879a79c37f074b0e37805309233ca8c0c
Jay Patel <jp9573@gmail.com>
2021-07-02 15:30:46 -0300

    Add PATCH API for book

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -47,9 +47,32 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
 
-      return res.json(book);
+      if (req.body._id) {
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
 
   return bookRouter;

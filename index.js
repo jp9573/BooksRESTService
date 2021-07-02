d4be536d4935f579045a91b753487ae8a56f9c2e
Jay Patel <jp9573@gmail.com>
2021-07-02 12:37:24 -0300

    Add book POST method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const Book = require("./models/bookModel");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,18 +10,28 @@ const port = process.env.PORT || 3000;
 const db = mongoose.connect(process.env.DATABASE_CONNECTION);
 const bookRouter = express.Router();
 
-bookRouter.route("/books").get((req, res) => {
-  let query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route("/books")
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    let query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
-});
 
 bookRouter.route("/books/:bookId").get((req, res) => {
   const { bookId } = req.params;
